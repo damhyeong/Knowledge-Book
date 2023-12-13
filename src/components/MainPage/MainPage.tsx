ab2a9c0290a894360a8e9a4d64ba2407fdeb1553
damhyeong <rhdwhdals8@naver.com>
2023-12-13 21:45:33 +0900

    Commit in local dev branch And Create Routes in MainPage

---
+++ src/components/MainPage/MainPage.tsx
@@ -0,0 +1,11 @@
+import react from 'react';
+import './styles.scss';
+
+const MainPage = () => {
+    return (
+        <div>
+            Testing MainPage at src/components/Mainpage/Mainpage.tsx
+        </div>
+    )
+}
+export default MainPage;
