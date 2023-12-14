364be6b5c3e832174dd4193aadef2cf48349c7bb
damhyeong <rhdwhdals8@naver.com>
2023-12-14 21:13:48 +0900

    Change Css And Adding some Animation in Text.

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -5,8 +5,10 @@ import {Link} from "react-router-dom";
 const MainPage = () => {
     return (
         <div className={"main-page-container"}>
-            <h2>Testing MainPage at src/components/Mainpage/Mainpage.tsx</h2>
-            <h1><Link to={"/Introduce/"}>Introduce Link!</Link></h1>
+            <div className={"text-container"}>
+                <h2>Testing MainPage at src/components/Mainpage/Mainpage.tsx</h2>
+                <h1><Link to={"/Introduce/"}>Introduce Link!</Link></h1>
+            </div>
         </div>
     )
 }
