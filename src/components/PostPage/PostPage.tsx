7918723cd88f5b4d5f9d848af03d001904ca35ac
damhyeong <rhdwhdals8@naver.com>
2023-12-28 20:51:57 +0900

    Caused Error in Local Space, but I think It may be working at the github Pages.

--- src/components/PostPage/PostPage.tsx
+++ src/components/PostPage/PostPage.tsx
@@ -45,23 +45,33 @@ const PostPage = () => {
 
     return (
         <div className={"post-page-container"}>
-            <div className={"post-page-header"}>
-                <div className={"post-title"}>
-                    {meta.title}
-                </div>
-                <div className={"post-date"}>
-                    {meta.date}
-                </div>
-                <div className={"post-keywords"}>
-                    {meta.keyword.map((keyword, index) => <PostKeyword keyword={keyword}/>)}
-                </div>
-            </div>
-            <hr/>
+
             <div className={"markdown-content"}>
                 <Markdown>{content}</Markdown>
             </div>
 
         </div>
     )
+
+    // return (
+    //     <div className={"post-page-container"}>
+    //         <div className={"post-page-header"}>
+    //             <div className={"post-title"}>
+    //                 {meta.title}
+    //             </div>
+    //             <div className={"post-date"}>
+    //                 {meta.date}
+    //             </div>
+    //             <div className={"post-keywords"}>
+    //                 {meta.keyword.map((keyword, index) => <PostKeyword keyword={keyword}/>)}
+    //             </div>
+    //         </div>
+    //         <hr/>
+    //         <div className={"markdown-content"}>
+    //             <Markdown>{content}</Markdown>
+    //         </div>
+    //
+    //     </div>
+    // )
 }
 export default PostPage;
